e4c157049c1f4360ef9fc4b7b37f6e1c23c0b5a4
David Mueller <drmuelr@gmail.com>
2017-03-01 08:11:16 -0500

    Added splash screen

---
+++ Assets/Scripts/SplashClear.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashClear : MonoBehaviour {
+
+	GameObject splashCanvas;
+
+	// Use this for initialization
+	void Start () {
+		splashCanvas = GameObject.Find("SplashCanvas");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Jump"))
+			ClearSplash();
+
+	}
+
+	void ClearSplash()
+	{
+		splashCanvas.SetActive(false);
+	}
+
+	void ShowSplash()
+	{
+		splashCanvas.SetActive(true);
+	}
+}
